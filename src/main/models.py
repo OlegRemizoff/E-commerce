a6dcb00b28b866664e7b797dc13d6d14aa41e94b
Oleg Remizov <job.rem1zoff@gmail.com>
2023-02-20 17:05:57 +0300

    create model Latest_Products

--- src/main/models.py
+++ src/main/models.py
@@ -5,6 +5,26 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+                
+        return products
+
+
+
+class LatestProducts:
+
+    objects = LatestProductsManager()
+
+
+
 class Customer(models.Model):
     '''Пользователь'''
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
