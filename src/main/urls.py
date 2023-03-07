a7882e11243a27b4723dac1d96c8844cbc6d486f
Oleg Remizov <job.rem1zoff@gmail.com>
2023-03-07 15:26:02 +0300

    overwrite index as view

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import index, ProductDetailView, CategoryDetailView
+from .views import HomeView, ProductDetailView, CategoryDetailView
 
 urlpatterns = [
-    path('', index, name="home"),
+    path('', HomeView.as_view(), name="home"),
     path('products/<str:ct_model>/<str:slug>/',
          ProductDetailView.as_view(), name="product_detail"),
     path('category/<slug:slug>/', CategoryDetailView.as_view(), name='category_detail')
--- src/main/views.py
+++ src/main/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, View
 from .models import SmartPhone, Notebook, LatestProducts, Category
 
 
-def index(request):
-    products = LatestProducts.objects.get_products_for_main_page(
-        "smartphone", "notebook")
-    return render(request, 'main/index.html', {"products": products})
+# def index(request):
+#     products = LatestProducts.objects.get_products_for_main_page(
+#         "smartphone", "notebook")
+#     return render(request, 'main/index.html', {"products": products})
+
+
+class HomeView(View):
+
+    def get(self, request, *args, **kwargs):
+        products = LatestProducts.objects.get_products_for_main_page(
+            "smartphone", "notebook")
+        return render(request, 'main/index.html', {"products": products})
 
 
 class ProductDetailView(DetailView):
@@ -27,8 +35,6 @@ class ProductDetailView(DetailView):
     context_object_name = "product"
     template_name = 'main/product_detail.html'
     slug_url_kwarg = 'slug'
-
-
 
 
 class CategoryDetailView(DetailView):
