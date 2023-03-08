2cc68c5bd7d29de7940293e722f8751985a7a0be
Oleg Remizov <job.rem1zoff@gmail.com>
2023-03-08 12:24:16 +0300

    show phones, notebooks by category

--- src/main/templatetags/tags_for_inc.py
+++ src/main/templatetags/tags_for_inc.py
@@ -1,8 +1,25 @@
 from django import template
-from main.models import Category
+from main.models import Category, SmartPhone, Notebook
 
 register = template.Library()
 
 @register.simple_tag()
 def get_categories():
     return Category.objects.get_category_for_navbar()
+
+
+@register.simple_tag()
+def get_smartphones():
+    return SmartPhone.objects.all()
+
+
+@register.simple_tag()
+def get_notebooks():
+    return Notebook.objects.all()
+
+
+
+
+
+
+
