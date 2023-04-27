9471e9d293ae0290861ecfe9951e7f73aa2beebe
Oleg Remizov <job.rem1zoff@gmail.com>
2023-04-27 10:09:10 +0300

    fixed cart and shitch on sqlite

--- src/main/context_processors/cart_processors.py
+++ src/main/context_processors/cart_processors.py
@@ -4,6 +4,19 @@ from main.models import Customer, Cart
 
 
 def get_cart_quantity(request):
-    customer = Customer.objects.get(user=request.user)
-    cart = Cart.objects.get(owner=customer)
+    # customer = Customer.objects.get(user=request.user)
+    # cart = Cart.objects.get(owner=customer)
+    # return {'cart': cart}
+
+    if request.user.is_authenticated:
+        customer = Customer.objects.filter(user=request.user).first()
+        if not customer:
+            customer = Customer.objects.create(user=request.user)
+        cart = Cart.objects.filter(owner=customer, in_order=False).first()
+        if not cart:
+            cart = Cart.objects.create(owner=customer)
+    else:
+        cart = Cart.objects.filter(for_anonymous_user=True).first()
+        if not cart:
+            cart = Cart.objects.create(for_anonymous_user=True)
     return {'cart': cart}
--- src/shop/settings.py
+++ src/shop/settings.py
@@ -79,17 +79,23 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ecommerce',
+#         'USER': 'oleg',
+#         'PASSWORD': '1234',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce',
-        'USER': 'oleg',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
